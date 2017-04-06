223478de7dda7f3aefabe33e6606b5671756e105
meltem kilic <melltemkilic@gmail.com>
2017-04-06 12:24:55 -0400

    for loop replaced with forEach

---
+++ callback2.js
@@ -0,0 +1,14 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  arr.forEach(function(element, index, array) {
+    if (element === "Waldo") {
+      found(index);   // execute callback
+    }
+  });
+}
+
+function actionWhenFound(index) {
+  console.log(`Found Waldo at index ${index}`);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
